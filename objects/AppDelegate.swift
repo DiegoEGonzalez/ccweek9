e8d3cb89bc41f675a43ded4cc11a385ee3583ec5
Diego Gonzalez <diego_gonzalez@filmmakernow.com>
2017-11-07 05:56:48 -0500

    Initial Commit

---
+++ objects/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  objects
+//
+//  Created by Diego E Gonzalez on 11/7/17.
+//  Copyright © 2017 Diego E Gonzalez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
